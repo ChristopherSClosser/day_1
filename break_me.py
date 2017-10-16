dd99e0a060974e3e63f8e935588026f57cf8da68
Chris Closser <chrisclosser1@gmail.com>
2017-10-16 16:32:19 -0700

    break me

--- break_me.py
+++ break_me.py
@@ -1,25 +1,67 @@
 """Break Me."""
 
-""" Create three functions:
-    name_error(): when called,
-        this function will cause a NameError
-    type_error(): when called,
-        this function will cause a TypeError
-    attribute_error(): when called,
-        this function will cause an AttributeError
-"""
-
 
 def name_error():
-    # return a name error
-    return
+    # return a name error by using an unassigned variable
+    name = input("What is your name? ")
+    print ("Hello ", my_name)
 
 
-def type_error():
-    # return a type error
-    return
+def type_error(a, b):
+    # return a type error by mixing str and int
+    print(a + "b" + b)
 
 
-def attribute_error():
-    # return an attribute error
-    return
+x = 5
+y = [1, 4, 5, ]
+
+
+def attribute_error(obj):
+    # return an attribute error by printing an attribute that is none
+    print(obj.key[0])
+
+
+k = {
+    "key": 1,
+    "key_2": [4, 5, 1, ],
+}
+
+
+# Break through 4 tracebacks
+def factorial(x):
+    # if x=3 | x=3*2*1
+    total = 1
+    while x > 0:
+        total *= x
+        x -= 1
+        print (total)
+        square(x)
+
+
+def is_prime(x):
+    x = '2'
+    if x < 2:
+        return False
+    else:
+        for n in range(2, x-1):
+            if x % n == 0:
+                return False
+            return True
+
+
+def square(x):
+    print(x ** 2)
+    print(is_prime(x))
+
+
+def trace_fail(x):
+    factorial(x)
+
+
+trace_fail(2837)
+
+
+# SyntaxError
+# def no_loop(obj):
+#    for item in obj --no colon
+#        print("Shouldn't see this.")
--- notes.md
+++ notes.md
@@ -1,12 +1,25 @@
 """mondays stuff"""
 
+I am a software craftsman
 
 # python 3 str is utf-8 unicode
 # python 2 division of integers results in a floored integer
-         # division of a float and an integer results in a float
+# python 2 division of a float and an integer results in a float
 # python 3 division of all integers results in a float
 
 
 # fridays learning journal due 9 am saturday
 
 # blog posting technical - and lighting talks
+
+
+Please read my current introduction to the core concepts of this course. Also spend some time browsing the published schedule for the course.
+
+When you’re done with the reading, please provide an answer for each of the following questions in the submission text area:
+
+  How does this approach sound to you?
+  What concerns do you have at this point?
+    failing at any one item
+  What topics listed are you most excited about?
+    learning to code better
+  What topics do you most want to learn about (whether listed or no)?
